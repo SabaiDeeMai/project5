d9709f9af0bd20352e135d9715753d049965dbb8
Denis <bekishev.denis@yandex.ru>
2025-03-31 20:40:04 +0700

    homework 10.2

--- src/masks.py
+++ src/masks.py
@@ -11,3 +11,5 @@ def get_mask_account(account_number: str) -> str:
 if __name__ == "__main__":
     print(get_mask_account(str(73654108430135874305)))
     print(get_mask_card_number(str(7000792289606361)))
+
+
