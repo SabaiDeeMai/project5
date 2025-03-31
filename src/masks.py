f0f7d51729f8638a1fcb6d7ae5d826032bd0965f
Denis <bekishev.denis@yandex.ru>
2025-03-31 15:47:23 +0700

    all changes + hw10.1

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску."""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску."""
+    return f"** {account_number[-4:]}"
+
+
+if __name__ == "__main__":
+    print(get_mask_account(str(73654108430135874305)))
+    print(get_mask_card_number(str(7000792289606361)))
