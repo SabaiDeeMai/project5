6b165a8f17cae4312eb7bd172f5c458c0edbaefb
Denis <bekishev.denis@yandex.ru>
2025-05-06 11:34:00 +0300

    add hw 12.2

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,47 @@
+import logging
+import os
+
+os.makedirs('../logs', exist_ok=True)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('../logs/masks.log', mode='w', encoding='utf-8'),
+    ]
+)
+
+masks_logger = logging.getLogger("masks_log")
+
+# masks_logger.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# file_handler = logging.FileHandler('../logs/masks.log', mode='w', encoding='utf-8')
+# file_handler.setFormatter(formatter)
+# masks_logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    masks_logger.debug(f"Получение номера карты: {card_number}")
+    if card_number.isdigit():
+        masks_logger.debug("Результат работы функции get_mask_card_number получен")
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        masks_logger.error("Карта состоит не из цифр")
+        return ""
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    return f"** {account_number[-4:]}"
+    masks_logger.debug(f"Получение номера карты: {account_number}")
+    if account_number.isdigit():
+        masks_logger.debug("Результат работы функции get_mask_account получен")
+        return f"** {account_number[-4:]}"
+    else:
+        masks_logger.error("Счет состоит не из цифр")
+        return ""
 
 
 if __name__ == "__main__":
-    print(get_mask_account(str(73654108430135874305)))
+    print(get_mask_account(str(7365410843013587430)))
     print(get_mask_card_number(str(7000792289606361)))
-
-
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,24 @@
 import json
+import logging
+import os
 from typing import Dict, List
+
+os.makedirs('../logs', exist_ok=True)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('../logs/utils.log', mode='w', encoding='utf-8'),
+    ]
+)
+
+utils_logger = logging.getLogger("utils_log")
+# utils_logger.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8')
+# file_handler.setFormatter(formatter)
+# utils_logger.addHandler(file_handler)
 
 
 def load_transactions(file_path: str) -> List[Dict]:
@@ -10,8 +29,12 @@ def load_transactions(file_path: str) -> List[Dict]:
     try:
         with open(file_path, 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
+            utils_logger.info(f"Файл получен: {file_path}")
             if isinstance(data, list):
+                utils_logger.info("Результат работы функции load_transactions получен")
                 return data
+            utils_logger.debug("Файл пустой")
             return []
     except (FileNotFoundError, json.JSONDecodeError):
+        utils_logger.error("Файл не найден")
         return []
